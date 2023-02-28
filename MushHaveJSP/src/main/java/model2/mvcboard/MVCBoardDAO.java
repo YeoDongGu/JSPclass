29f64dd0d3e43581563f1cfd220c109f1b043a79
Administrator <nanovia3@naver.com>
2023-02-28 18:09:05 +0900

    123

--- MushHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
+++ MushHaveJSP/src/main/java/model2/mvcboard/MVCBoardDAO.java
@@ -160,4 +160,67 @@ public class MVCBoardDAO extends DBConnPool {
     		e.printStackTrace();
     	}
     }
+    
+    // 입력한 비밀번호가 지정한 일련번호의 게시물의 비밀번호와 일치하는지 확인합니다.
+    public boolean confirmPassword(String pass, String idx) {
+        boolean isCorr = true;
+        try {
+            String sql = "SELECT COUNT(*) FROM mvcboard WHERE pass=? AND idx=?";
+            psmt = con.prepareStatement(sql);
+            psmt.setString(1, pass);
+            psmt.setString(2, idx);
+            rs = psmt.executeQuery();
+            rs.next();
+            if (rs.getInt(1) == 0) {
+                isCorr = false;
+            }
+        }
+        catch (Exception e) {
+            isCorr = false;
+            e.printStackTrace();
+        }
+        return isCorr;
+    }
+
+    // 지정한 일련번호의 게시물을 삭제합니다.
+    public int deletePost(String idx) {
+        int result = 0;
+        try {
+            String query = "DELETE FROM mvcboard WHERE idx=?";
+            psmt = con.prepareStatement(query);
+            psmt.setString(1, idx);
+            result = psmt.executeUpdate();
+        }
+        catch (Exception e) {
+            System.out.println("게시물 삭제 중 예외 발생");
+            e.printStackTrace();
+        }
+        return result;
+    }
+    
+    public int updatePost(MVCBoardDTO dto) {
+    	int result = 0;
+    	try {
+    		String query = "update mvcboard"
+    					+ " SET title=?, name=?, content=?, ofile=?, sfile=? "
+    					+ " where idx=? and pass=?";
+    		
+    		psmt = con.prepareStatement(query);
+    		psmt.setString(1, dto.getTitle());
+    		psmt.setString(2, dto.getName());
+    		psmt.setString(3, dto.getContent());
+    		psmt.setString(4, dto.getOfile());
+    		psmt.setString(5, dto.getSfile());
+    		psmt.setString(6, dto.getIdx());
+    		psmt.setString(7, dto.getPass());
+    		
+    		result = psmt.executeUpdate();
+
+    		
+    	}catch(Exception e) {
+    		System.out.println("게시물 수정 중 에외 발생");
+    		e.printStackTrace();
+    	}
+    	return result;
+    }
 }
--- MushHaveJSP/src/main/java/model2/mvcboard/PassController.java
+++ MushHaveJSP/src/main/java/model2/mvcboard/PassController.java
@@ -1,11 +1,16 @@
 package model2.mvcboard;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fileupload.FileUtil;
+import utils.JSFunction;
 
 @WebServlet("/mvcboard/pass.do")
 public class PassController extends HttpServlet {
@@ -15,6 +20,39 @@ public class PassController extends HttpServlet {
 			throws ServletException, IOException {
 		request.setAttribute("mode", request.getParameter("mode"));
 		request.getRequestDispatcher("/14MVCBoard/Pass.jsp").forward(request, response);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		//매개변수 저장
+		String idx = request.getParameter("idx");
+		String mode = request.getParameter("mode");
+		String pass = request.getParameter("pass");
+		
+		//비밀번호 확인
+		MVCBoardDAO dao = new MVCBoardDAO();
+		boolean confirmed = dao.confirmPassword(pass, idx);
+		dao.close();
+		
+		if(confirmed) {
+			if(mode.equals("edit")) { //수정 모드
+				HttpSession session = request.getSession();
+				session.setAttribute("pass", pass);
+				response.sendRedirect("../mvcboard/edit.do?idx=" + idx);
+			} else if(mode.equals("delete")) { //삭제 모드
+				dao = new MVCBoardDAO();
+				MVCBoardDTO dto = dao.selectView(idx);
+				int result = dao.deletePost(idx);
+				dao.close();
+				if(result ==1) {
+					String saveFileName = dto.getSfile();
+					FileUtil.deleteFile(request, "/Uploads", saveFileName);
+				}
+				JSFunction.alertLocation(response, "삭제되었습니다.", "../mvcboard/list.do");
+			}
+		}else { //비밀번호 불일치
+			JSFunction.alertBack(response, "비밀번호 검증에 실패했습니다.");
+		}
 	}
 
 }
